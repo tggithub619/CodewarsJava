4d4d8784ffc396fd22276b537d26b57c1ca74596
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-14 23:26:36 -0700

    1

---
+++ src/lucky_number.java
@@ -0,0 +1,11 @@
+public class lucky_number {
+
+    public static boolean isLucky(long n) {
+        return (n%9==0) ? true : false;
+    }
+
+
+}
+
+
+//https://www.codewars.com/kata/55afed09237df73343000042/train/java
