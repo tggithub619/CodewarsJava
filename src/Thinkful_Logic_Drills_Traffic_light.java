fd50c132fc6caf2e5ec7e812f76a92ead2676192
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-02 13:41:55 -0800

    2

--- src/Thinkful_Logic_Drills_Traffic_light.java
+++ src/Thinkful_Logic_Drills_Traffic_light.java
@@ -1,13 +1,22 @@
 public class Thinkful_Logic_Drills_Traffic_light {
 
-    public static String updateLight(String current) {
-        if ( current.equals("red")){
-            return "green";
-        } else if  (current.equals("green")){
-            return "yellow";
-        }
+//    public static String updateLight(String current) {
+//        if ( current.equals("red")){
+//            return "green";
+//        } else if  (current.equals("green")){
+//            return "yellow";
+//        }
+//
+//        return "red";
+//    }
 
-        return "red";
+    public static String updateLight(String current) {
+        switch (current) {
+            case "red": return "green";
+            case "yellow": return "red";
+            case "green": return "yellow";
+            default: throw new IllegalArgumentException();
+        }
     }
 
     public static void main(String[] args) {
