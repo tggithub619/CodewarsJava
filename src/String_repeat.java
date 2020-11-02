6d2d703ec3d2dd7e3723349f7f5466b3823d12b8
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-02 13:28:00 -0800

    1

--- src/String_repeat.java
+++ src/String_repeat.java
@@ -1,5 +1,7 @@
 public class String_repeat {
 
+//https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/solutions/java
+
     public static String repeatStr(final int repeat, final String string) {
         String res = "";
         for (int i = 0; i <repeat; i++) {
