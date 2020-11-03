0d1b66a44799bb6c1a6ddd31ec29de2dd4eccec4
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-03 14:23:36 -0800

    1

---
+++ src/RemoveFirstAndLastCharacter.java
@@ -0,0 +1,8 @@
+public class RemoveFirstAndLastCharacter {
+
+    //https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/java
+    public static String remove(String str) {
+
+        return str.substring(1, str.length() -1);
+    }
+}
