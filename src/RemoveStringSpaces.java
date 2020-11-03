1bfecc74b84c089ef7896b2f9c1d677884b836a6
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-03 14:34:30 -0800

    1

---
+++ src/RemoveStringSpaces.java
@@ -0,0 +1,8 @@
+public class RemoveStringSpaces {
+
+    //https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/java
+
+    static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
