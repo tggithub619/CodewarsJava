45cb94c7a93d9426a61f9e6bff358d72524ac5fe
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-03 12:47:20 -0800

    1

---
+++ src/NumberofDecimalDigits.java
@@ -0,0 +1,22 @@
+public class NumberofDecimalDigits {
+
+    public static int Digits(long n) {
+        if (n < 10) {
+            return 1;
+        }
+        int count = 0;
+        long last;
+        while (n != 0){
+            last = n % 10;
+            n = (n-last)/10;
+            count++;
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Digits(5l));
+        System.out.println(Digits(12345l));
+    }
+}
