3ea669adcea71d828c0870a44989735f684fb534
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-03 14:17:10 -0800

    1

---
+++ src/MakeUpperCase.java
@@ -0,0 +1,12 @@
+public class MakeUpperCase {
+
+    //https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/java
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MakeUpperCase("hello"));
+    }
+}
