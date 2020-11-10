c360ca194c35c2f6aff3667d90819d88c4f7b93a
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-09 17:27:29 -0800

    2

--- src/ExesAndOhs.java
+++ src/ExesAndOhs.java
@@ -22,4 +22,11 @@ public class ExesAndOhs {
         }
         return o == x;
     }
+
+
+    // public static boolean getXO (String str) {
+    //    str = str.toLowerCase();
+    //    return str.replace("o","").length() == str.replace("x","").length();
+    //
+    //  }
 }
