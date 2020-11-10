60f618b21ae00ac46fffa04d2c7c2711989ed427
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-10 15:00:36 -0800

    2

---
+++ src/ConvertAStringToArray.java
@@ -0,0 +1,10 @@
+public class ConvertAStringToArray {
+
+    //https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/java
+
+    public static String[] stringToArray(String s) {
+        String[] arr = s.split(" ");
+        return arr;
+    }
+
+}
