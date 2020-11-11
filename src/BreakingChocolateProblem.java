74b454315e50d80c01902755b6bd95e2e017cd4d
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-11 14:59:06 -0800

    2

--- src/BreakingChocolateProblem.java
+++ src/BreakingChocolateProblem.java
@@ -5,6 +5,7 @@ public class BreakingChocolateProblem {
     public static int breakChocolate(int n, int m) {
         if(n == 0 || m == 0) return 0;
         return n*m - 1;
+        // return Math.max(n * m - 1, 0);
     }
 
 }
